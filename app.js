37913c188ac89e3ca4577b701e0c0a8e7855a164
Christian Goldman <christian.goldman9638@gmail.com>
2020-12-09 12:51:15 -0600

    inquier prompt finished, building html with user input

--- app.js
+++ app.js
@@ -9,7 +9,127 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employeArray = [];
 
+function test() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "What is your role?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your ID?",
+      },
+    ])
+    .then(function (answers) {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "office",
+            message: "What is your office Number?",
+            when: () => answers.role === "Manager",
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+            when: () => answers.role === "Engineer",
+          },
+          {
+            type: "input",
+            name: "school",
+            message: "What school did you attend?",
+            when: () => answers.role === "Intern",
+          },
+        ])
+        .then(function (specAnswer) {
+          console.log(answers.role);
+          switch (answers.role) {
+            case "Engineer":
+              const engineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                specAnswer.github
+              );
+              employeArray.push(engineer);
+              testAgain();
+              break;
+
+            case "Intern":
+              const intern = new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                specAnswer.school
+              );
+              employeArray.push(intern);
+              testAgain();
+              break;
+
+            case "Manager":
+              const manager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                specAnswer.office
+              );
+              employeArray.push(manager);
+              testAgain();
+              break;
+            default:
+              console.log(employeArray);
+              testAgain();
+          }
+        });
+    });
+}
+function testAgain() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "again",
+        message: "Would you like to add another employee?",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then(function (again) {
+      if (again.again === "Yes") {
+        test();
+      } else {
+        //checking to see if output folder not existed
+        if(!fs.existsSync(OUTPUT_DIR)){
+          fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFile(outputPath, render(employeArray), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+}
+
+test();
+
+// do first prompt get answers
+// 2nd inquirer prompt inside of first .then function
+// if statement checks what role selected then displays corresponding string
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
